26990c2fce52236786e5ec47bebec24977b3dced
Bernhard Reitinger <bernhard@reitinger.eu>
2025-01-13 08:54:13 +0100

    neovim: add hack for Obsidian

--- nvim/.config/nvim/ftplugin/markdown.lua
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -1,5 +1,8 @@
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
 
+-- See here: https://github.com/epwalsh/obsidian.nvim/issues/286
+vim.opt_local.conceallevel = 1
+
 vim.keymap.set("n", "j", "gj", { buffer = true, silent = true })
 vim.keymap.set("n", "k", "gk", { buffer = true, silent = true })
